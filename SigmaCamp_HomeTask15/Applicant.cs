a0bd30c1e69a2fb05ebb15cae6d28560d27b8e2e
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-08-02 17:13:29 +0300

    Added homeTask15

---
+++ SigmaCamp_HomeTask15/Applicant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaCamp_HomeTask15
+{
+    internal class Applicant
+    {
+        private string _surname;
+        private int _entryYear;
+        private int _schoolNumber;
+        public  int SchoolNumber {
+            get { return _schoolNumber; }
+            set
+            {
+                if(value <= 0) throw new ArgumentOutOfRangeException();
+                _schoolNumber = value;
+            }
+        }
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                if(string.IsNullOrEmpty(value)) throw new ArgumentNullException();
+                _surname = value;
+            }
+        }
+        public int EntryYear
+        {
+            get { return _entryYear; }
+            set
+            {
+                if(value <= 0) throw new ArgumentOutOfRangeException();
+                _entryYear = value;
+            }
+        }
+        public Applicant(string name = default, int schoolNum = default, int entryYear = default)
+        {
+            Surname = name;
+            SchoolNumber = schoolNum;
+            EntryYear = entryYear;
+        }
+    }
+}
---
+++ SigmaCamp_HomeTask15/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SigmaCamp_HomeTask15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>()
+            {
+                4,
+                1,
+                7,
+                5
+            };
+            List<string> lines = new List<string>()
+            {
+                //"kjeeerj",
+                //"9",
+                //"63u3u",
+                //"0gdkgjs433"
+                "ffhdjfhdj",
+                "3kj3k3j",
+                "338",
+                "fsmdsff",
+                "43",
+                "jy"
+            };
+            //Print<string>(GetHW15_1Result(nums, lines));
+            Print<(char, int)>(GetHW15_2Result(lines));
+        }
+        static List<string> GetHW15_1Result(List<int> nums, List<string> lines)
+        {
+            return nums.Select(num => lines
+            .Where(line => Char.IsDigit(line[0]) && line.Length == num)
+            .ToList().FirstOrDefault() ?? "Не знайдено")
+                .ToList();
+        }
+        static List<(char, int)> GetHW15_2Result(List<string> lines)
+        {
+            return lines.GroupBy(line => line[0])
+                .Select(g => new { KeySign = g.Key, TotalLength = g.Sum(line => line.Length) })
+                .OrderByDescending(a => a.TotalLength)
+                .ThenBy(a => a.KeySign)
+                .Select(a => (KeySign: a.KeySign, TotalLength: a.TotalLength))
+                .ToList();
+        }
+        static List<(int, int)> GetHW15_3Result(List<Applicant> applicants, List<int> years)
+        {
+            return years.Select(year => (Year: year, 
+                NumberOfSchools: applicants.Where(a => a.EntryYear == year)
+                .Select(a => a.SchoolNumber)
+                .Distinct()
+                .Count()))
+                .OrderBy(r => r.NumberOfSchools)
+                .ThenBy(r => r.Year)
+                .ToList();
+        }
+        static Dictionary<int, List<string>> GetHW15_4Result(List<Applicant> applicants)
+        {
+            return applicants.Select(a => a.EntryYear).ToDictionary(year => year, year => applicants.Where(a => a.EntryYear == year).Select(a => a.Surname).ToList());
+        }
+        static void Print<T>(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
